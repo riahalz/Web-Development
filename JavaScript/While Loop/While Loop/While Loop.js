52c53a7123964eb1fd849059beec0eba8e455b60
Riya Halbhavi <halbhaviriya@gmail.com>
2024-11-13 11:48:23 +0530

    Add files via upload

---
+++ JavaScript/While Loop/While Loop/While Loop.js
@@ -0,0 +1,8 @@
+// While Loop
+
+let val = 1;
+while (val<6)
+{
+	document.write(val);
+	val += 1
+}
---
+++ JavaScript/While Loop/do-while loop/do-while loop.js
@@ -0,0 +1,9 @@
+// do-while loop (exit control loop)
+
+let test = 1;
+do
+{
+	document.write(test)
+	test ++;
+}
+while(test <= 5)
